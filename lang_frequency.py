e0efa23954b3bb3bd87114830af0d1fb8480f700
UpmostScarab <fuzzykiller2@gmail.com>
2016-09-19 17:57:36 +0300

    Вынес число в константу

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,5 +1,6 @@
 import re
 from collections import Counter
+MOST_COMMON_WORDS_COUNT = 10
 
 def load_data(filepath):
     with open(filepath, 'r') as text_file:
@@ -13,4 +14,4 @@ def get_most_frequent_words(text):
 
 if __name__ == '__main__':
     text = load_data(input('Введите путь к файлу с текстом: '))
-    print(get_most_frequent_words(text).most_common(10))
+    print(get_most_frequent_words(text).most_common(MOST_COMMON_WORDS_COUNT))
